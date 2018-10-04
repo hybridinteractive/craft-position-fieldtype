cd1cf4307c7ed51393d865f513d2664b7749af93
Simon D. Müller <simon.d.mueller@gmail.com>
2018-10-04 22:09:12 +0000

    added german translation

---
+++ src/translations/de/position-fieldtype.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Position Fieldtype plugin for Craft CMS 3.x.
+ *
+ * Brings back the Position fieldtype from Craft 2
+ *
+ * @link      https://rias.be
+ *
+ * @copyright Copyright (c) 2017 Rias
+ */
+
+/**
+ * Position Fieldtype de Translation.
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('position-fieldtype', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Rias
+ *
+ * @since     1.0.0
+ */
+return [
+    'Left'                                                             => 'Links',
+    'Right'                                                            => 'Rechts',
+    'Options'                                                          => 'Optionen',
+    'Center'                                                           => 'Mitte',
+    'Position'                                                         => 'Position',
+    'Drop-right'                                                       => 'Rechts andocken',
+    'Drop-left'                                                        => 'Links andocken',
+    'Choose which position options should be available to your field.' => 'Wähle welche positionen für dein Feld verfügbar sein sollen.',
+    'Full'                                                             => 'Ganz',
+    'Default'                                                          => 'Standart',
+    'Choose a default position'                                        => '"Normale" Position wählen',
+];
